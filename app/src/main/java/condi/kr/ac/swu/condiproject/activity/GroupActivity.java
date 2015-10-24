93bcb110ab202a148cb8b0d041db1a426132991a
2sssol <sollee032699@gmail.com>
2015-10-24 13:12:14 +0900

    ok

---
+++ app/src/main/java/condi/kr/ac/swu/condiproject/activity/GroupActivity.java
@@ -0,0 +1,18 @@
+package condi.kr.ac.swu.condiproject.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import condi.kr.ac.swu.condiproject.R;
+
+public class GroupActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+
+}
